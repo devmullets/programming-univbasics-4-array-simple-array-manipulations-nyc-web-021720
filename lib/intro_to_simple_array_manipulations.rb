c4a49dac689aeb091d572e20afc046727ab16f2c
Ryan Vaznis <ryan.vaznis@gmail.com>
2020-02-03 00:45:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,7 +2,7 @@ def using_push (array, newElement)
   array.push(newElement)
 end
 
-def using_unshift(array, removeElement)
-  array.unshift(removeElement)
+def using_unshift(array, newElement)
+  array.unshift(newElement)
 end
 
